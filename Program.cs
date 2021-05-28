1aa5b72507b9270f2efcff94b092f6de4c339fc7
Yulia Rokotova <74153328+Rodluvis@users.noreply.github.com>
2021-05-28 12:04:19 +0200

    Calculations tested and verified on mock data.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,13 @@ namespace G4HE
             var mock = new NewMock();
             mock.MockIncome();
             mock.MockExpenditures();
-            Console.WriteLine($"Money lft this month: {BC.MoneyLeft()} ");
+            Console.WriteLine($"Income this month: {Math.Round(BC.TotalIncome(),2)} ");
+            Console.WriteLine($"Total expenses this month: {Math.Round(BC.TotalExpenses(),2)} ");
+            Console.WriteLine($" - of which fixed expenses this month: {Math.Round(BC.FixedExpenses(),2)} ");
+            Console.WriteLine($" - of which savings this month: {Math.Round(BC.Savings(), 2)} ");
+            Console.WriteLine($" - of which unexpected expenses this month: {Math.Round(BC.UnexpectedExpenses(), 2)} ");
+            Console.WriteLine($"Calculated expenses (savings + unexpected expenses) : {Math.Round(BC.CalculatedExpenses(), 2)} ");
+            Console.WriteLine($"Money left this month: {Math.Round(BC.MoneyLeft(), 2)} ");
 
         }
 
